c1deb218d05c819e930abd379e250e0ac9715954
CloudPassenger <coliwatoshinzo@gmail.com>
2024-04-03 13:22:09 +0100

    fix: path in README

--- bm7_converter.sh
+++ bm7_converter.sh
@@ -70,7 +70,7 @@ done
 echo "📝 Formatting: README.md"
 output_root_readme="$output_dir/README.md"
 if [ -f "$output_root_readme" ]; then
-    sed -i 's|https://github.com/blackmatrix7/ios_rule_script/tree/master/rule/|https://github.com/CloudPassenger/sing-rules/tree/ruleset/|g' "$output_root_readme"
+    sed -i 's|https://github.com/blackmatrix7/ios_rule_script/tree/master/rule/Clash/|https://github.com/CloudPassenger/sing-rules/tree/ruleset/|g' "$output_root_readme"
 fi
 
 echo "🎉 All files copied!"
